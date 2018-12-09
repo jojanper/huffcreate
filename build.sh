892134f7195e6ae0dc41bc752799cdd8a3777181
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-12-09 11:53:49 +0200

    Travis CI setup (#1)

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ BUILD_DIR=${2-'build'}
 
 rm -Rf ${BUILD_DIR}/*
 mkdir -p ${BUILD_DIR}
-cmake -H. -B./${BUILD_DIR} -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+cmake -H. -B./${BUILD_DIR} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
 cd ${BUILD_DIR}
 cmake --build .
